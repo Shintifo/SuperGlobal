383fc4c3efcb2f7269dbca615f272a1881c7c2cd
t <t.brayko@innopolis.university>
2024-06-22 21:24:11 +0800

    Working 2-imgs ONNX model + formatting

--- core/checkpoint.py
+++ core/checkpoint.py
@@ -12,25 +12,23 @@ import os
 import torch
 from ..config import cfg
 
+
 def load_checkpoint(checkpoint_file, model):
-    """Loads the checkpoint from the given file."""
-    err_str = "Checkpoint '{}' not found"
-    assert os.path.exists(checkpoint_file), err_str.format(checkpoint_file)
-    # Load the checkpoint on CPU to avoid GPU mem spike
-    checkpoint = torch.load(checkpoint_file, map_location="cpu")
-    try:
-        state_dict = checkpoint["model_state"]
-    except KeyError:
-        state_dict = checkpoint
-    
-    
-    model_dict = model.state_dict()
-    state_dict = {k : v for k, v in state_dict.items()}
-    weight_dict = {k : v for k, v in state_dict.items() if k in model_dict and model_dict[k].size() == v.size()}
+	"""Loads the checkpoint from the given file."""
+	err_str = "Checkpoint '{}' not found"
+	assert os.path.exists(checkpoint_file), err_str.format(checkpoint_file)
+	# Load the checkpoint on CPU to avoid GPU mem spike
+	checkpoint = torch.load(checkpoint_file, map_location="cpu")
+	try:
+		state_dict = checkpoint["model_state"]
+	except KeyError:
+		state_dict = checkpoint
 
-    
+	model_dict = model.state_dict()
+	state_dict = {k: v for k, v in state_dict.items()}
+	weight_dict = {k: v for k, v in state_dict.items() if k in model_dict and model_dict[k].size() == v.size()}
 
-    model_dict.update(weight_dict)
-    model.load_state_dict(model_dict)
+	model_dict.update(weight_dict)
+	model.load_state_dict(model_dict)
 
-    return checkpoint
+	return checkpoint
